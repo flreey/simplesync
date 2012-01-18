7e759b8c239d65482d95349b4f75f0c791e63cba
flreey <flreey@gmail.com>
2012-01-18 16:29:56 +0800

    client and server demo

--- client/synclient.py
+++ client/synclient.py
@@ -3,3 +3,4 @@ from gevent.socket import socket
 sock = socket()
 sock.connect(('localhost', 3000))
 sock.send('haha')
+print sock.recv(1000)
--- server/syncserver.py
+++ server/syncserver.py
@@ -1,12 +1,27 @@
+import gevent
+from gevent import spawn, sleep
 from gevent.server import StreamServer
 from gevent.pool import Pool
 
 def handle(socket, address):
     print socket.recv(1000)
-    while True:
-        import time
-        print 'ha'
-        time.sleep(1)
+    contents = readfile()
+    socket.send(contents)
+    #spawn(p)
+    #gevent.joinall([spawn(p)], 0)
+    #sleep(0)
 
-server = StreamServer(('localhost', 3000), handle, spwan=Pool(10))
+def readfile():
+    import glob
+    fs = glob.glob('*')
+    contents = ''
+    for f in fs:
+        with open(f, 'r') as ff:
+            contents += contents.join(ff.readlines())
+
+    print contents
+    return contents
+
+pool = Pool(1000)
+server = StreamServer(('localhost', 3000), handle, spawn=pool)
 server.serve_forever()
